37f7f6d3f46853e3eb95aafcda6d8b08ce06349b
HabebaMohamedd12 <habeba2007294@miuegypt.edu.eg>
2023-03-11 14:18:46 +0200

    editing

---
+++ demo/src/main/java/com/example/test.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.apache.catalina.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+
+public class test {
+    @GetMapping ("/signup1")
+    public String signupForm(){
+   return "signup.html";
+    }
+   @PostMapping ("/user")
+   public String homePage (@ModelAttribute User user ){
+    return "redirect:/home.html";
+   }
+}
